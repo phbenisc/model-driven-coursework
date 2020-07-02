6524d6525db36e8d81ecced588bb4a6c90e30521
Philip Yannick Benischke <philip.benischke@gmail.com>
2020-07-02 16:43:15 +0200

    feat: added generation

--- runtime/src/main/scala/de/htwg/konstanz/modelling/generator/Generator.scala
+++ runtime/src/main/scala/de/htwg/konstanz/modelling/generator/Generator.scala
@@ -1,5 +1,26 @@
 package de.htwg.konstanz.modelling.generator
 
+import de.htwg.konstanz.modelling.parser.Parser
+import de.htwg.konstanz.modelling.parser.Transformer
+import de.htwg.konstanz.modelling.templates.xml.Records
+
+import scala.reflect.io.File
+import scala.xml.{PrettyPrinter, XML}
+
 class Generator {
 
+  def generateFile(dsl: String): Unit = {
+
+    val parser = new Parser
+
+    for(
+      record <- Transformer.apply(parser.parseDSL(dsl))
+    ) {
+      val string = Records.render(record).toString.stripLeading()
+      val xml = XML.loadString(string)
+      val p = new PrettyPrinter(80, 4)
+
+      File("record.xsd").writeAll(p.format(xml))
+    }
+  }
 }
---
+++ runtime/src/test/scala/de/htwg/konstanz/modelling/generator/GeneratorTest.scala
@@ -0,0 +1,25 @@
+package de.htwg.konstanz.modelling.generator
+
+import de.htwg.konstanz.modelling.parser.Parser
+import org.junit.jupiter.api.Test
+
+class GeneratorTest {
+  val sut = new Generator()
+
+  @Test
+  def testComplexDSL(): Unit = {
+
+    val c0RecordDsl =
+      """Record baseAmount {
+        |     Field required variety:        C          pos ( 1,  1) as fixedValue C
+        |     Field required type:           C          pos (42,  1) as fixedValue 0
+        |
+        |     Field required conditionId:    Zc         pos ( 2,  3) as fixedValue 000
+        |
+        |     Field sign:                    C          pos (45,  1) as enum { +, - }
+        |     Field amount:                  N (10, 5)  pos (46, 15)
+        |}""".stripMargin
+
+      sut.generateFile(c0RecordDsl)
+  }
+}
